3ee21fd9605a3972d8d8a8ddf8ad7eaff7821a70
Sai Veerendra Kotipalli <84443587+Sai-Veerendra@users.noreply.github.com>
2022-10-29 11:23:21 +0530

    def pal(num):     sum=0     while num>0:         r=num%10         sum=sum*10+r         num=num//10     return sum n=int(input()) if n==pal(n): #pal(n)-->sum     print("True") else:     print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,12 @@
+def pal(num):
+    sum=0
+    while num>0:
+        r=num%10
+        sum=sum*10+r
+        num=num//10
+    return sum
 n=int(input())
-t=n
-sum=0
-while n>0:
-    r=n%10
-    sum= sum*10 + r
-    n=n//10
-if t==sum:
+if n==pal(n): #pal(n)-->sum
     print("True")
 else:
     print("False")
